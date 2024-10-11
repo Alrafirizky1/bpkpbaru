b731d3dc08400d7f63cdeede091383a02c93aa52
Alrafi <Malrafirizky@gmail.com>
2024-10-11 10:58:47 +0700

    rafifirst

---
+++ cetak_disposisi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Auto Print</title>
+    <script type="text/javascript">
+        window.onload = function() {
+            window.print();
+        }
+    </script>
+</head>
+<body>
+    <h1>Surat Sudah Diterima</h1>
+    <p>Silakan cetak dokumen ini untuk arsip Anda.</p>
+</body>
+</html>
